9bd7ccfcd2d1983d6b09ad08214d011e1ab66cc2
exalotto <dev@exalotto.eth>
2025-01-25 20:26:09 +0100

    Add status.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 import { BrowserProvider, JsonRpcProvider, type Provider } from 'ethers';
 import { useWeb3React, Web3ReactProvider } from '@web3-react/core';
@@ -9,14 +9,13 @@ import { InjectedConnector } from '@web3-react/injected-connector';
 import { Button } from '@headlessui/react';
 
 import { TokenSale } from './TokenSale';
+import Link from 'next/link';
 
 const TokenSaleContext = createContext<TokenSale | null>(null);
 
-const useTokenSale = () => useContext(TokenSaleContext);
-
 const SalePanel = () => {
   const requiredChainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID!, 10);
-  const { active, chainId, library, activate } = useWeb3React<Provider>();
+  const { active, chainId, account, library, activate } = useWeb3React<Provider>();
   const [tokenSale, setTokenSale] = useState<TokenSale | null>(null);
   const [isOpen, setOpen] = useState<boolean | null>(null);
   useEffect(() => {
@@ -42,7 +41,19 @@ const SalePanel = () => {
   return (
     <TokenSaleContext.Provider value={tokenSale}>
       <>
-        {active ? null : (
+        {active ? (
+          <p>
+            Connected as{' '}
+            <Link
+              href={`https://${process.env.NEXT_PUBLIC_BLOCK_EXPLORER}/address/${account}`}
+              rel="noreferrer"
+              target="_blank"
+            >
+              {account}
+            </Link>{' '}
+            to network with ID {chainId} via {process.env.NEXT_PUBLIC_RPC_URL}
+          </p>
+        ) : (
           <p>
             <Button
               onClick={() => {
@@ -53,6 +64,16 @@ const SalePanel = () => {
             </Button>
           </p>
         )}
+        <p>
+          TokenSale contract address:{' '}
+          <Link
+            href={`https://${process.env.NEXT_PUBLIC_BLOCK_EXPLORER}/address/${process.env.NEXT_PUBLIC_TOKEN_SALE_ADDRESS}`}
+            rel="noreferrer"
+            target="_blank"
+          >
+            {process.env.NEXT_PUBLIC_TOKEN_SALE_ADDRESS}
+          </Link>
+        </p>
         {isOpen !== null ? <p>Status: {isOpen ? 'OPEN' : 'CLOSE'}</p> : null}
       </>
     </TokenSaleContext.Provider>
